60a39f8e4c35af606ff1e5ba9d77d1e49bc9b690
javsort <javierortega336@gmail.com>
2023-12-15 01:41:29 +0100

    Finished

--- cw.js
+++ cw.js
@@ -3,68 +3,68 @@
 const cwlib = require( './cwlib.js' );
 
 // Declare main variables to be used
-let line = '';
+let lineRead = '';
 let lineIndex = 0;
 let longestSubstrings = [];
 let longestSubLength = 0;
 
 // Check if string is balanced
-function isBalanced(str) {
+function isBalanced(toBalance) {
     // Make array of 26 elements - for the alphabet
-    let counts = Array(26).fill(0);
+    let balance = Array(26).fill(0);
 
     // For each character in the string, increment or decrement the count at the index of the character
-    str.split('').forEach(char => {
+    toBalance.split('').forEach(char => {
         // Get the index of the character in the alphabet
-        let index = char.toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0);
+        let val = char.toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0);
 
         // If the index is valid, increment or decrement the count at the index depending on if it's upper or lower case
-        if (index >= 0 && index < 26) {
-            counts[index] += (char === char.toUpperCase()) ? 1 : -1;
+        if (val >= 0 && val < 26) {
+            balance[val] += (char === char.toUpperCase()) ? 1 : -1;
         }
 
     });
 
     // If all counts are 0, the string is balanced (true), else it's false
-    return counts.every(count => count === 0);
+    return balance.every(balance => balance === 0);
 }
 
 // function to find all substrings in a line, and return the largest
-function findSubstrings(line) {
+function findSubstrings(lineRead) {
     // declare the array to be returned
     let returnVal = [];
 
     // For each character in the line, get all substrings starting with that character
-    returnVal = Array(line.length).fill().flatMap((_, i) => 
-    Array(line.length - i + 1).fill().map((_, j) => {
+    returnVal = Array(lineRead.length).fill().flatMap((_, i) => 
+    Array(lineRead.length - i + 1).fill().map((_, j) => {
 
         // Get the substring
-        let sliced = line.slice(i, i + j);
+        let sliced = lineRead.slice(i, i + j);
 
         // Return the substring as an object. Contains the the start and end index from the original array, and the line where it came from
         return {substring: sliced, indexStart: i, indexFinish: i + j - 1, lineIndex: lineIndex};
     })
-    ).filter(sub => sub.substring.length > 0);          // Filter out empty substrings
+    ).filter(toDel => toDel.substring.length > 0);          // Filter out empty substrings
 
     // Filter out substrings that aren't balanced
-    returnVal = returnVal.filter(sub => isBalanced(sub.substring));
+    returnVal = returnVal.filter(current => isBalanced(current.substring));
 
     // Sort the substrings by size for printing
     returnVal.sort((a, b) => (a.indexFinish - a.indexStart) - (b.indexFinish - b.indexStart));
 
     // Print the substrings found to console
-    returnVal.forEach(sub => {
-        console.log("[ " + sub.indexStart + ", " + sub.indexFinish + " ]");
+    returnVal.forEach(toPrint => {
+        console.log("[ " + toPrint.indexStart + ", " + toPrint.indexFinish + " ]");
     
     });
 
     // If it's already sorted by size, just get the last one and return all with the same size
-    let longest = returnVal.filter(sub => sub.substring.length === returnVal[returnVal.length - 1].substring.length);
+    let longest = returnVal.filter(item => item.substring.length === returnVal[returnVal.length - 1].substring.length);
     
     // Turn the longest substrings into a string for printing
     let longestInLine = "";
-    longest.forEach(sub => {
-        longestInLine += "[ " + sub.indexStart + ", " + sub.indexFinish + " ], "; 
+    longest.forEach(toPrint => {
+        longestInLine += "[ " + toPrint.indexStart + ", " + toPrint.indexFinish + " ], "; 
     });
 
     // Remove the last comma and space and print the longest substrings for this line
@@ -106,18 +106,18 @@ cwlib.on( 'ready', function( ) {
 // For each character, add it to the line
 cwlib.on( 'data', function( data ) {
     // Add data to line
-    line += data;
+    lineRead += data;
 
 } );
 
 // When the line is reset, find the substrings and update the longest
 cwlib.on( 'reset', function() {
-    let substrings = findSubstrings(line);
+    let substrings = findSubstrings(lineRead);
 
     updateLongest(substrings);
 
     // Reset the line and increment the line index
-    line = '';
+    lineRead = '';
     lineIndex++;
 });
 
@@ -126,7 +126,7 @@ cwlib.on( 'end', function() {
     let subStringsFinal = printSubStrings(longestSubstrings);
 
     // Print overall longest substrings
-    console.log("Overall longest balanced substrings: ",subStringsFinal);
+    console.log("Overall longest balanced substrings: ", subStringsFinal);
 } );
 
 // function to print the substrings
@@ -140,17 +140,17 @@ function printSubStrings(subStrings) {
     let currentLineIndex = subStrings[0].lineIndex;
     let currentLineStrings = "";
 
-    subStrings.forEach(sub => {
+    subStrings.forEach(currtItem => {
         // If the substring is on the same line as the current line, add it to the current line string
-        if(sub.lineIndex === currentLineIndex){
-            currentLineStrings += "[ " + sub.indexStart + ", " + sub.indexFinish + " ], ";
+        if(currtItem.lineIndex === currentLineIndex){
+            currentLineStrings += "[ " + currtItem.indexStart + ", " + currtItem.indexFinish + " ], ";
             
             // If the substring is on a different line, add the current line index and string to the final string, and reset the current line index and string
         } else {
             finalString += "[ " + currentLineIndex + ", [ " + currentLineStrings.slice(0, -2) + " ] ]\n" ;
-            currentLineIndex = sub.lineIndex;
+            currentLineIndex = currtItem.lineIndex;
             currentLineStrings = "";
-            currentLineStrings += "[ " + sub.indexStart + ", " + sub.indexFinish + " ], ";
+            currentLineStrings += "[ " + currtItem.indexStart + ", " + currtItem.indexFinish + " ], ";
 
         }
     });
